39cae7ec9dc89b8ada0e1e1c71d5a8154885086e
Ulrik Lyngs <ulrik.lyngs@cs.ox.ac.uk>
2020-03-02 12:36:13 +0000

    remove public folder - let netlify build this

--- public/js/navbar-hide.js
+++
@@ -1,7 +0,0 @@
-// scroll functions
-$(window).scroll(function(e) {
-
-    // add/remove class to navbar when scrolling to hide/show
-    $('.navbar')[$(window).scrollTop() >= 150 ? 'addClass' : 'removeClass']('navbar-hide');
-
-});
